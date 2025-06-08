022c0f359581795607a9f53693b918fffc8488c2
MaliikHassanSaeed <malikhassani890@gmail.com>
2025-06-08 18:23:24 +0500

    changes

--- src/app/component/CartModal.tsx
+++ src/app/component/CartModal.tsx
@@ -73,7 +73,6 @@ try{
           <div>
             <div className="flex items-center justify-between font-semibold">
               <span>Subtotal</span>
-              <span>${cart.subtotal.amount}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">Shipping and taxes calculated at checkout</p>
             <div className="flex justify-between text-sm">
